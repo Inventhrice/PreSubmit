4e68e583aeab9357e61a8065730e8fdb88d90ed3
Inventhrice <avaneesha03@gmail.com>
2023-11-11 21:22:10 -0500

    Add the uploadFile code and experiemented with it

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+function uploadFile() {
+    // Get the input element
+    var input = document.getElementById('fileInput');
+
+    // Check if a file is selected
+    if (input.files.length > 0) {
+        // Access the selected file
+        var file = input.files[0];
+
+        // Display file details
+        console.log('File name: ' + file.name);
+        console.log('File type: ' + file.type);
+        console.log('File size: ' + file.size + ' bytes');
+    } else {
+        console.log('No file selected');
+    }
+}
