8b5315ce576d88f548d76e12189c8010a7db8ba1
Inventhrice <avaneesha03@gmail.com>
2023-11-15 21:14:23 -0500

    Added all the bootstrap code to begin making it look pretty

--- scripts/baseTemplate.js
+++ scripts/baseTemplate.js
@@ -1,6 +1,17 @@
-var template =`        <a href="index.html">Index</a><br>
-        <a href="templates.html">Templates</a><br>
-        <a href="Settings.html">Settings</a><br>
-        <a href="about.html">About</a><br>`
+var template =`
+<div class="row h">
+    <div class="col">
+        <h1><a href="index.html">Pre-Submit</a></h1>
+        <header class="d-flex justify-content-center">
+            <ul class="nav nav-pills">
+            <li class="nav-item"><a href="index.html" class="nav-link">Index</a><br></li>
+            <li class="nav-item"><a href="templates.html" class="nav-link">Templates</a><br></li>
+            <li class="nav-item"><a href="Settings.html" class="nav-link">Settings</a><br></li>
+            <li class="nav-item"><a href="about.html" class="nav-link">About</a><br></li>
+            </ul>
+        </header>
+    </div>
+<div>
+`
 
 document.getElementById('template').innerHTML = template
