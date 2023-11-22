a985f23544f7530d4ff9d69e0c6b94db910e5043
Inventhrice <avaneesha03@gmail.com>
2023-11-21 22:22:45 -0500

    Worked on it a little bit!

---
+++ scripts/indexTemplate.js
@@ -0,0 +1,7 @@
+var templateCode = `
+<div id="row h">            
+    <a href="templateEditor.html">Create New Template</a>
+</div>
+`
+
+document.getElementById('listTemplates').innerHTML = templateCode
