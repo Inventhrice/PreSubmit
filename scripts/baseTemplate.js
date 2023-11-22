ffdbb46049900e95c768efd900d555d418696cbb
Inventhrice <45127310+Inventhrice@users.noreply.github.com>
2023-11-22 15:32:11 -0500

    Version 0.1.0: First Iteration of Features (#2)
    * Made the starting files
    
    * Add the uploadFile code and experiemented with it
    
    * Added the comments button
    
    * Allowed multiple files to be selected
    
    * Added another few pages, beginning to construct the TODO lists on what to do
    
    * Added links to the other pages
    
    * Added fileUpload, which will be the page where users can upload the files
    
    * Moved the file upload code out of index.html
    
    * Added a results.html page to display the results
    
    * Added the rest of the pages and created templates for all of them
    
    * Made the template code all extendable, along with providing links to all of the pages

---
+++ scripts/baseTemplate.js
@@ -0,0 +1,6 @@
+var template =`        <a href="index.html">Index</a><br>
+        <a href="templates.html">Templates</a><br>
+        <a href="Settings.html">Settings</a><br>
+        <a href="about.html">About</a><br>`
+
+document.getElementById('template').innerHTML = template
