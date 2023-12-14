19ac636634bb1b2e649f3931863b69838d030a12
Inventhrice <avaneesha03@gmail.com>
2023-12-13 22:34:00 -0500

    Initalized a Vue project and removed all the inital files that does not pertain to what I needed

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+  export default{
+    data(){
+      return{
+        message: "Hello world!"
+      }
+    }
+  }
+</script>
+
+<template>
+  <body>
+    <h1> {{ message  }}</h1>
+  </body>
+</template>
+
+<style scoped>
+
+</style>
