3dd798edd31e246e3db4a1aba371af9b279de926
Inventhrice <45127310+Inventhrice@users.noreply.github.com>
2023-12-14 01:12:57 -0500

    Vue integration (#3)
    * Removed all previous files to start revamping it into a Vue application
    
    * Initalized a Vue project and removed all the inital files that does not pertain to what I needed
    
    * Translated the header code into something usable, imported bootstrap, updated gitignore
    
    * Catching up to the Second Iteration branch
    
    * Got most of the logic figured out. Just have to figure out why the active logic isn't working.
    
    * Figured it out: turns out the auto assignation of ids just kept on incrementing

--- scripts/baseTemplate.js
+++
@@ -1,6 +0,0 @@
-var template =`        <a href="index.html">Index</a><br>
-        <a href="templates.html">Templates</a><br>
-        <a href="Settings.html">Settings</a><br>
-        <a href="about.html">About</a><br>`
-
-document.getElementById('template').innerHTML = template
