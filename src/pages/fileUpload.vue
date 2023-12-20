dbd00e95e2df818c681894e89ea02ec6d528ff45
Inventhrice <avaneesha03@gmail.com>
2023-12-19 22:25:26 -0500

    Figured out how to read a file! Big progress!

--- src/pages/fileUpload.vue
+++ src/pages/fileUpload.vue
@@ -30,13 +30,28 @@ import DisplayResults from '../components/DisplayResults.vue';
                 var input = document.getElementById('fileInput');
                 // Check if a file is selected
                 if (input.files.length > 0) {
+                    const reader = new FileReader()
+                    
+                    reader.addEventListener(
+                        "load",
+                        () => {
+                        // this will then display a text file
+                        //this.test = reader.result
+                        this.showResult = true
+                        },
+                        false,
+                    );
+
                     // Access the selected file
                     var file = input.files[0];
-
-                    // Display file details
-                    console.log('File name: ' + file.name);
-                    console.log('File type: ' + file.type);
-                    console.log('File size: ' + file.size + ' bytes');
+                    if(file){
+                        reader.readAsText(file)
+                        // Display file details
+                        console.log('File name: ' + file.name);
+                        console.log('File type: ' + file.type);
+                        console.log('File size: ' + file.size + ' bytes');
+                    }
+                    
                 } else {
                     console.log('No file selected');
                 }
