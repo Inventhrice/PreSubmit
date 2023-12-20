33b31feda3266dc6bf6b21bf3ef957024cf28f09
Inventhrice <avaneesha03@gmail.com>
2023-12-19 22:02:41 -0500

    Figured out how to write and call a method

--- src/pages/fileUpload.vue
+++ src/pages/fileUpload.vue
@@ -3,39 +3,46 @@
     <input type="file" id="fileInput" accept=".txt, .zip" multiple><br><br>
     <label>Comments: </label><br>
     <textarea v-model="comments" placeholder="Put your comments here"></textarea><br><br>
-    <button @click="showResult = !showResult">Check my submission!</button><br><br>
-    <div v-if="this.showResult === true">
-        <displayResults :score="40"/>
+    <button @click="analyze">Check my submission!</button><br><br>
+    <div v-if="showResult === true">
+        <displayResults :score="score"/>
     </div>
 </template>
 
 <script>
 import DisplayResults from '../components/DisplayResults.vue';
+
+function uploadFile() {                
+    // Get the input element
+    var input = document.getElementById('fileInput');
+    // Check if a file is selected
+    if (input.files.length > 0) {
+        // Access the selected file
+        var file = input.files[0];
+
+        // Display file details
+        console.log('File name: ' + file.name);
+        console.log('File type: ' + file.type);
+        console.log('File size: ' + file.size + ' bytes');
+    } else {
+        console.log('No file selected');
+    }
+}
+
     export default{
         data(){
             return{
                 showResult: false,
-                comments: ""
+                comments: "",
+                score: 70
             }
         },
         methods: {
-            uploadFile() {
+            analyze(event){
+                this.showResult = !this.showResult
+                let totalscore = 100
+                this.score = totalscore - 10
                 
-                // Get the input element
-                var input = document.getElementById('fileInput');
-                var comments = document.getElementById('comments');
-                // Check if a file is selected
-                if (input.files.length > 0) {
-                    // Access the selected file
-                    var file = input.files[0];
-
-                    // Display file details
-                    console.log('File name: ' + file.name);
-                    console.log('File type: ' + file.type);
-                    console.log('File size: ' + file.size + ' bytes');
-                } else {
-                    console.log('No file selected');
-                }
             }
         },
         components:{
