43db0884fe02f42e683df93fcc50471f54a8b05c
Inventhrice <avaneesha03@gmail.com>
2023-12-31 22:06:53 -0500

    Added the buttons, comments, just fixing the removeCondition bug as it doesnt work

--- src/pages/templateEditor.vue
+++ src/pages/templateEditor.vue
@@ -18,6 +18,11 @@ export default{
     methods:{
         addCondition(){
             this.listOfConditions.push({conditionName: "File/Folder", fName: "", isFolder: false, isExist: false})
+        },
+        removeCondition(condition){
+            console.log(this.listOfConditions)
+            this.listOfConditions = this.listOfConditions.filter((t) => t !== condition)
+            console.log(this.listOfConditions)
         }
     }
 }
@@ -25,23 +30,35 @@ export default{
 
 <template>
     <div id="row h">
+        <!-- Renders Comments -->
         <h2>{{ comments.conditionName }}</h2>
         <input type="checkbox" :value="comments.isEmpty" @input="comments.isEmpty = !comments.isEmpty">
         <label>&nbsp;Check this box if the comments should be empty</label>
         <div v-if="!comments.isEmpty"><textarea v-model="comments.contains" placeholder="What should the comments say?"></textarea><br></div>
 
+        <!-- Displays if the list of conditions is empty -->
+        <div v-if="listOfConditions.length === 0"><br><button @click="addCondition">Add condition</button><br></div>
+
+        <!-- Iterates through all the list of conditions and renders -->
         <div v-for="singleCondition in listOfConditions">
             <br>
-            <h2>{{ singleCondition.conditionName }}</h2>
+            <!-- Does three things: renders condition name; if its the last in list: have a plus button to add another condition, and have a minus button to remove condition -->
+            <h2>{{ singleCondition.conditionName }} <button v-if="singleCondition === listOfConditions[listOfConditions.length-1]" @click="addCondition">+</button><button @click="removeCondition(singleCondition)">-</button></h2>
+            
+            <!-- Changes between file and folder, and asks for name -->
             <input type="checkbox" :value="singleCondition.isFolder" @input="singleCondition.isFolder = !singleCondition.isFolder">
             <label>&nbsp;Name of the {{singleCondition.isFolder ? "Folder" : "File"}}:&nbsp;</label>
             <input v-if="singleCondition.isFolder" :value="singleCondition.fName" @input="singleCondition.fName = this.target.value" placeholder="Enter name of folder here">
             <input v-else :value="singleCondition.fName" @input="singleCondition.fName = this.target.value" placeholder="Enter name of file here">
+            
             <br>
+            
+            <!-- Asking if the file/folder should exist -->
             <input type="checkbox" :value="singleCondition.isExist" @input="singleCondition.isExist = !singleCondition.isExist">
             <label>&nbsp;Check this box if the {{singleCondition.isFolder ? "folder" : "file"}} should exist</label>
         </div>
     </div>
+    <br>
     <router-link to="/fileUpload" v-slot="{href, navigate}">
         <button :href="href" @click="navigate">Next</button>
     </router-link>
